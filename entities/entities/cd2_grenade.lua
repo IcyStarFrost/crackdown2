5bc03ddb89b7b9f6b535647726a53a7dffb8b0c9
IcyStarFrost <frostlegion10@gmail.com>
2022-12-28 16:15:12 -0600

    Reduced grenade cooldown

--- entities/entities/cd2_grenade.lua
+++ entities/entities/cd2_grenade.lua
@@ -4,6 +4,7 @@ ENT.Base = "cd2_equipmentbase"
 ENT.WorldModel = "models/weapons/w_grenade.mdl"
 ENT.PrintName = "Frag Grenade"
 
+ENT.Cooldown = 1
 ENT.MaxGrenadeCount = 8
 ENT.TrailColor = Color( 255 ,174, 0)
 ENT.DelayTime = 3 -- The time before the grenade blows up. 0 for no timed explosive

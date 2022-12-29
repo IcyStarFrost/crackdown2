06816c20a419199cac2b431725c41fd9186ab834
IcyStarFrost <frostlegion10@gmail.com>
2022-12-29 11:18:47 -0600

    Rewrote pickup system

--- gamemode/crackdown2/sh_pickupsystem.lua
+++ gamemode/crackdown2/sh_pickupsystem.lua
@@ -11,7 +11,7 @@ local isphysics = {
 
 local delay = 0
 
-hook.Add( "Tick", "crackdown2_pickupsystem", function()
+--[[ hook.Add( "Tick", "crackdown2_pickupsystem", function()
 
     for k, ply in ipairs( player_GetAll() ) do
         if !ply:IsCD2Agent() then continue end
@@ -101,6 +101,106 @@ hook.Add( "Tick", "crackdown2_pickupsystem", function()
         
     end
     
+end ) ]]
+
+-- New rewritten pick up system
+hook.Add( "Tick", "crackdown2_pickupsystem", function()
+    if CLIENT then return end
+    local players = player_GetAll()
+
+    for i = 1, #players do
+        local ply = players[ i ]
+        if !ply:IsCD2Agent() then continue end
+
+        local near = CD2FindInSphere( ply:GetPos(), 100, function( ent ) return isphysics[ ent:GetClass() ] and ent:GetNW2Int( "cd2_mass", math.huge ) < ply:GetMaxPickupWeight() and ply:Visible( ent ) end )
+        ply:SetNW2Entity( "cd2_pickuptarget", CD2GetClosestInTable( near, ply ) )
+
+        -- Held key
+        if ply:KeyDown( IN_USE ) then ply.cd2_PickupDelay = ply.cd2_PickupDelay or CurTime() + 1 else ply.cd2_PickupDelay = nil end
+
+        -- Pickup a new object
+        if !IsValid( ply.cd2_HeldObject ) and IsValid( ply:GetNW2Entity( "cd2_pickuptarget", nil ) ) and ( ply.cd2_PickupDelay and CurTime() > ply.cd2_PickupDelay ) then
+            ply:GetActiveWeapon():EnterPickupMode()
+            local ent = ply:GetNW2Entity( "cd2_pickuptarget", nil )
+
+            ent:SetPos( ply:HandsPos() )
+            ent:SetAngles( ply:HandsAngles() )
+
+            if !ent:IsRagdoll() then
+                ent:SetParent( ply, ply:LookupAttachment( "anim_attachment_RH" ) )
+            end
+
+            ent:SetOwner( ply )
+            ent:SetCollisionGroup( COLLISION_GROUP_IN_VEHICLE )
+
+            ply:SetNW2Bool( "cd2_isholdingent", true )
+            ply.cd2_HeldObject = ent
+            ply.cd2_PickupDelay = math.huge -- Prevent the delayed code from running again until the use key is lifted
+
+        elseif IsValid( ply.cd2_HeldObject ) and ( ply.cd2_PickupDelay and CurTime() > ply.cd2_PickupDelay or !ply:Alive() ) then -- Drop the held object
+
+            local ent = ply.cd2_HeldObject
+            ent:SetParent()
+            ent:SetPos( ply:HandsPos() )
+            ent:SetCollisionGroup( COLLISION_GROUP_NONE )
+            ent:SetAngles( ply:HandsAngles() )
+            ent:PhysWake()
+
+            timer.Simple( 1, function() if !IsValid( ent ) then return end ent:SetOwner( NULL ) end )
+
+            ply:SetNW2Bool( "cd2_isholdingent", false )
+            if IsValid( ply:GetActiveWeapon() ) then ply:GetActiveWeapon():ExitPickupMode() end
+            ply.cd2_HeldObject = nil
+            ply.cd2_PickupDelay = math.huge -- Prevent the delayed code from running again until the use key is lifted
+
+        elseif IsValid( ply.cd2_HeldObject ) and ply:KeyPressed( IN_ATTACK ) and !ply:KeyDown( IN_USE ) then -- Throw the held object
+
+            ply:SetAnimation( PLAYER_ATTACK1 )
+
+            local ent = ply.cd2_HeldObject
+            ent:SetParent()
+            ent:SetCollisionGroup( COLLISION_GROUP_NONE )
+            ent:SetPos( ply:HandsPos() )
+            ent:SetAngles( ply:HandsAngles() )
+            ent:PhysWake()
+            if SERVER then ent:SetPhysicsAttacker( ply, 2 ) end
+            
+            local phys = ent:GetPhysicsObject()
+
+            if IsValid( phys ) then
+                local mult = ent:IsRagdoll() and 800000 or 1500
+                phys:ApplyForceCenter( ply:GetAimVector() * ( phys:GetMass() * mult ) )
+            end
+
+            ply:SetNW2Bool( "cd2_isholdingent", false )
+            timer.Simple( 1, function()
+                if IsValid( ply ) and IsValid( ply:GetActiveWeapon() ) then
+                    ply:GetActiveWeapon():ExitPickupMode() 
+                end
+                if IsValid( ent ) then 
+                    ent:SetOwner( NULL ) 
+                end
+            end )
+            ply.cd2_HeldObject = nil
+        end
+
+        -- Code to set ragdoll's position to the player's hands
+        if IsValid( ply.cd2_HeldObject ) and ply.cd2_HeldObject:IsRagdoll() then
+            local ent = ply.cd2_HeldObject
+            local phys = ent:GetPhysicsObject()
+
+            if IsValid( phys ) then
+                phys:SetPos( ply:HandsPos() )
+            end
+        end
+
+        if !IsValid( ply.cd2_HeldObject ) and ply:GetNW2Bool( "cd2_isholdingent", false ) then
+            ply:SetNW2Bool( "cd2_isholdingent", false )
+            ply:GetActiveWeapon():ExitPickupMode()
+        end
+
+    end
+
 end )
 
 if CLIENT then
@@ -110,8 +210,12 @@ if CLIENT then
 
     hook.Add( "HUDPaint", "crackdown2_pickupprompt", function()
         local ply = LocalPlayer()
-        local targ = ply.cd2_Pickuptarget
-        if !IsValid( targ ) or IsValid( ply.cd2_HeldTarget ) then return end
+
+        local targ = ply:GetNW2Entity( "cd2_pickuptarget", nil )
+        local isholding = ply:GetNW2Bool( "cd2_isholdingent", false )
+
+        if !IsValid( targ ) or isholding then return end
+
         local usebind = input_LookupBinding( "+use" ) or "e"
         local code = input_GetKeyCode( usebind )
         local buttonname = input_GetKeyName( code )

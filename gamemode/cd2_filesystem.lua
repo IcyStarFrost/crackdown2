c225e48e9effbe0c03e41e5f79c100df6f7ffbc6
IcyStarFrost <frostlegion10@gmail.com>
2022-12-30 14:04:04 -0600

    Added Agent data

--- gamemode/cd2_filesystem.lua
+++ gamemode/cd2_filesystem.lua
@@ -4,12 +4,14 @@ local compress = util.Compress
 local decompress = util.Decompress
 print( "Crackdown2 Console: File System has been loaded")
 
+file.CreateDir( "crackdown2" )
+
 -- The gamemode's custom file system. This is what handles the saving, loading, and general data players have
 CD2FILESYSTEM = {}
 
-
+-- File writing with the ability to easily pack tables into jsons or compressed jsons
 function CD2FILESYSTEM:Write( filename, contents, type )
-
+    CD2DebugMessage( "Wrote to file " .. filename )
 	local f = file.Open( filename, type == "compressed" and "wb" or "w", "DATA" )
 	if ( !f ) then return end
 
@@ -25,7 +27,9 @@ function CD2FILESYSTEM:Write( filename, contents, type )
 
 end
 
+-- File reading with the ability to directly get a table from a json or compressed json
 function CD2FILESYSTEM:Read( filename, type )
+    CD2DebugMessage( "Reading from file " .. filename )
 
 	local f = file.Open( filename, type == "compressed" and "rb" or "r", "DATA" )
 	if ( !f ) then return end
@@ -41,4 +45,24 @@ function CD2FILESYSTEM:Read( filename, type )
     end
 
 	return contents
+end
+
+
+if CLIENT then
+
+    -- Writes a value to the Player's agent data
+    function CD2FILESYSTEM:WritePlayerData( var, any )
+        CD2DebugMessage( "Writing to " .. LocalPlayer():Name() .. "'s Agent data: var = " .. var .. " | value = " .. tostring( any ) )
+        local data = CD2FILESYSTEM:Read( "crackdown2/agentdata.dat", "compressed" )
+        data[ var ] = any
+        CD2FILESYSTEM:Write( "crackdown2/agentdata.dat", data, "compressed" )
+    end
+
+    -- Reads a value from the Player's agent data
+    function CD2FILESYSTEM:ReadPlayerData( var )
+        local data = CD2FILESYSTEM:Read( "crackdown2/agentdata.dat", "compressed" )
+        return data[ var ]
+    end
+
+    CD2FILESYSTEM:Write( "crackdown2/agentdata.dat", {}, "compressed" )
 end

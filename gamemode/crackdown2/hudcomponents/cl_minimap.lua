bb7b20944c4bfb2dfe761218c6d227f51a32faa0
IcyStarFrost <frostlegion10@gmail.com>
2025-03-07 01:13:18 -0600

    Update the minimap

--- gamemode/crackdown2/hudcomponents/cl_minimap.lua
+++ gamemode/crackdown2/hudcomponents/cl_minimap.lua
@@ -1,5 +1,4 @@
 -- Draws the minimap
-local blackish = Color( 39, 39, 39)
 local downicon = Material( "crackdown2/ui/down.png", "smooth" )
 local cellicon = Material( "crackdown2/ui/celltrackericon.png" )
 local pingmat = Material( "crackdown2/ui/pingcircle.png" )
@@ -31,51 +30,54 @@ function CD2:RemovePingLocation( id )
     ping_locations[ id ] = nil
 end
 
-local function WorldVectorToScreen2( pos, origin, rotation, scale, radius )
-    local relativePosition = pos - origin
+-- Translates a 3d position to the minimap
+local function ToMinimap( pos )
+    local rotation = CD2.viewangles[ 2 ] - 90
 
-    relativePosition:Rotate( Angle( 0, -rotation, 0 ) )
+    pos.z = 0
+    local origin = LocalPlayer():GetPos()
+    origin.z = 0
 
-    local angle = math.atan2( relativePosition.y, relativePosition.x )
+    local relativepos = pos - origin
+    
+    relativepos = relativepos * 0.06 -- Minimap scaling
+
+    relativepos:Rotate( Angle( 0, -rotation, 0 ) )
+
+    -- Get the angle from the relative vector
+    local angle = math.atan2( relativepos.y, relativepos.x )
     angle = math.deg( angle )
 
-    local distance = relativePosition:Length()
+    -- Circular coordinates
+    local x = math.cos( math.rad( angle ) ) * relativepos:Length()
+    local y = math.sin( math.rad( angle ) ) * relativepos:Length()
 
-    local x = math.cos( math.rad( angle ) ) * distance * scale
-    local y = math.sin( math.rad( angle ) ) * distance * scale
+    local sqr = math.sqrt( x ^ 2 + y ^ 2 )
 
-    sqr = math.sqrt( x ^ 2 + y ^ 2 )
-
-    if sqr > radius then
-        local angle = math.atan2( y, x )
-        x = math.cos( angle ) * radius
-        y = math.sin( angle ) * radius
+    -- Limit within the minimap's bounds
+    if sqr > 150 then
+        x = math.cos( math.rad( angle ) ) * 150
+        y = math.sin( math.rad( angle ) ) * 150
     end
 
     return Vector( x, y )
 end
 
-local function DrawCoordsOnMiniMap( pos, ang, icon, iconsize, color, fov )
-    local radius = ScreenScale( 45 )
-    pos[ 3 ] = 0
-    local plypos = LocalPlayer():GetPos() plypos[ 3 ] = 0
-    
+local function DrawCoordsOnMiniMap( pos, ang, icon, iconsize, color)
     local _, angs = WorldToLocal( pos, Angle( 0, ang[ 2 ], 0 ), LocalPlayer():GetPos(), CD2.viewangles )
 
     surface.SetDrawColor( color or color_white )
     surface.SetMaterial( icon or playerarrow )
 
-    local vec = WorldVectorToScreen2( pos, plypos, CD2.viewangles[ 2 ] - 90, radius / ( fov * 170 ), radius ) --WorldVectorToScreen( pos, plypos, 0.2, CD2.viewangles[ 2 ] - 90, radius, fov )
+    local vec = ToMinimap( pos )
     surface.DrawTexturedRectRotated( 200 + vec[ 1 ], ( ScrH() - 200 ) - vec[ 2 ], ScreenScale( iconsize ), ScreenScale( iconsize ), ( angs[ 2 ] ) )
 end
 
 function CD2.HUDCOMPONENENTS.components.Minimap( ply, scrw, scrh, hudscale )
     if !ply:Alive() then return end
-    
-    local fov = 15
 
     draw.NoTexture()
-    surface.SetDrawColor( blackish )
+    surface.SetDrawColor( 39, 39, 39, 200 )
     CD2:DrawCircle( 200, scrh - 200, ScreenScale( 50 ), 30 )
 
     -- { id = uniqueid, pos = pos, times = times, ping_scale = 0, can_ping = true, times_pinged = 0, persist = persist }
@@ -86,7 +88,7 @@ function CD2.HUDCOMPONENENTS.components.Minimap( ply, scrw, scrh, hudscale )
         end
 
         ping.ping_scale = Lerp( 3.5 * FrameTime(), ping.ping_scale, ping.times > ping.times_pinged and 40 or 20 )
-        DrawCoordsOnMiniMap( ping.pos, CD2.viewangles, pingmat, ping.ping_scale, cellwhite, fov )
+        DrawCoordsOnMiniMap( ping.pos, CD2.viewangles, pingmat, ping.ping_scale, cellwhite )
 
         if ping.can_ping and ping.times > ping.times_pinged then
             surface.PlaySound( "crackdown2/ui/ping.mp3" )
@@ -112,7 +114,7 @@ function CD2.HUDCOMPONENENTS.components.Minimap( ply, scrw, scrh, hudscale )
         local ent = nearbyminimap[ i ]
         local z = ent:GetPos().z
         local icon = z > ply:GetPos().z + 50 and upicon or z < ply:GetPos().z - 50 and downicon or cellicon
-        DrawCoordsOnMiniMap( ent:GetPos(), CD2.viewangles, icon, 4, ent:GetEnemy() == ply and celltargetred or cellwhite, fov )
+        DrawCoordsOnMiniMap( ent:GetPos(), CD2.viewangles, icon, 4, ent:GetEnemy() == ply and celltargetred or cellwhite )
     end
     --
     
@@ -123,15 +125,15 @@ function CD2.HUDCOMPONENENTS.components.Minimap( ply, scrw, scrh, hudscale )
         local ent = ents_[ i ]
 
         if IsValid( ent ) and ent:GetClass() == "cd2_locationmarker" and ( ent:SqrRangeTo( LocalPlayer() ) < ( 6000 * 6000 ) or ent:GetLocationType() == "beacon" ) then 
-            DrawCoordsOnMiniMap( ent:GetPos(), Angle( 0, CD2.viewangles[ 2 ], 0 ), ent:GetLocationType() == "beacon" and beaconicon or ent:GetLocationType() == "cell" and cell or peacekeeper, ent:GetLocationType() == "beacon" and 20 or 10, ent:GetLocationType() == "cell" and celltargetred or color_white, fov )
+            DrawCoordsOnMiniMap( ent:GetPos(), Angle( 0, CD2.viewangles[ 2 ], 0 ), ent:GetLocationType() == "beacon" and beaconicon or ent:GetLocationType() == "cell" and cell or peacekeeper, ent:GetLocationType() == "beacon" and 20 or 10, ent:GetLocationType() == "cell" and celltargetred or color_white )
         elseif IsValid( ent ) and ent:GetClass() == "cd2_agencyhelicopter" and ent:SqrRangeTo( LocalPlayer() ) < ( 6000 * 6000 ) then
-            DrawCoordsOnMiniMap( ent:GetPos(), ent:GetAngles(), heloicon, 15, color_white, fov )
+            DrawCoordsOnMiniMap( ent:GetPos(), ent:GetAngles(), heloicon, 15, color_white )
         elseif IsValid( ent ) and ent:GetClass() == "cd2_au" and !ent:GetActive() then
-            DrawCoordsOnMiniMap( ent:GetPos(), Angle( 0, CD2.viewangles[ 2 ], 0 ), Auicon, 10, color_white, fov )
+            DrawCoordsOnMiniMap( ent:GetPos(), Angle( 0, CD2.viewangles[ 2 ], 0 ), Auicon, 10, color_white )
         elseif IsValid( ent ) and ent:GetClass() == "cd2_towerbeacon" and !ent:GetIsDetonated() and ent:CanBeActivated() then
-            DrawCoordsOnMiniMap( ent:GetPos(), Angle( 0, CD2.viewangles[ 2 ], 0 ), staricon, 10, beaconblue, fov )
+            DrawCoordsOnMiniMap( ent:GetPos(), Angle( 0, CD2.viewangles[ 2 ], 0 ), staricon, 10, beaconblue )
         elseif IsValid( ent ) and ent:IsCD2NPC() and ent:GetCD2Team() == "freak" and IsValid( ent:GetEnemy() ) and ( ( ent:GetEnemy():GetNWBool( "cd2_beaconpart", false ) or ent:GetEnemy():GetClass() == "cd2_beacon" ) or ( ent:GetEnemy():GetNWBool( "cd2_towerbeaconpart", false ) ) ) then
-            DrawCoordsOnMiniMap( ent:GetPos(), Angle( 0, CD2.viewangles[ 2 ], 0 ), FreakIcon, 10, color_white, fov )
+            DrawCoordsOnMiniMap( ent:GetPos(), Angle( 0, CD2.viewangles[ 2 ], 0 ), FreakIcon, 10, color_white )
         end
     end
     --
@@ -142,7 +144,7 @@ function CD2.HUDCOMPONENENTS.components.Minimap( ply, scrw, scrh, hudscale )
     for i = 1, #players do
         local otherplayer = players[ i ]
         if IsValid( otherplayer ) and otherplayer:IsCD2Agent() and otherplayer != ply then
-            DrawCoordsOnMiniMap( otherplayer:GetPos(), otherplayer:EyeAngles(), otherplayer:Alive() and playerarrow or agentdown, 10, otherplayer:GetPlayerColor():ToColor(), fov )
+            DrawCoordsOnMiniMap( otherplayer:GetPos(), otherplayer:EyeAngles(), otherplayer:Alive() and playerarrow or agentdown, 10, otherplayer:GetPlayerColor():ToColor() )
         end
     end
     --
